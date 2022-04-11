e70df7874ebf128420801dc3a3ddaac41c71e199
))c3x <95924648+0336893201@users.noreply.github.com>
2022-04-12 01:15:23 +0700

    Add files via upload

---
+++ Project163.cpp
@@ -0,0 +1,127 @@
+// Project163.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+#include <math.h>
+int main()
+{
+    int a_num[]{ 1,6,9,3,1,2,3,7 };
+    std::cout << a_num[1] << std::endl;
+
+    if (a_num[1] < 10) {
+        a_num[1] / 2;
+        a_num[0] / 100;
+    }
+    else
+    {
+
+        false;
+
+    }
+    int O[]{ 3,6,9,10,32,600,300,100,9100 };
+    char s[] = "M";
+    char* d_string = s;
+    cout << "p_string = " << d_string << endl;
+    int* C = &O[3];
+    std::cout << "MY RESULT IS = " << C << endl;
+    for (int s = 0; s < 10; s++) {
+        char r[] = "HI THERE J36281";
+        char* b = &r[0];
+        cout << O + s << "=" << " " << *C + s << endl;
+        if (s < 10) {
+            break;
+
+        }
+        else {
+
+
+            cout << b + s << "=" << " " << *r + s * 2 << endl;
+
+        }
+
+    }
+
+
+
+
+
+    //POINTER:CON TRO LA CACH GAN CAC DIA CHI CHO CAC BIEN(SO,KY TU,V.V)KHI CHUNG DUOC LUU TRONG MOT BO NHO
+           //GIONG VOI VIEC CO VE XEM PHIM(CHO NGOI,SO GHE,KHOANG CACH,GIO CHIEU,SO NGUOI)*/
+
+
+    {
+        cout << "Hello World!\n" << endl;
+        int e;
+        e = 16;
+        cin >> e;
+        cout << "hIIIIIIIIIIIIII!\n";
+        int s;
+        //s=number 1
+        int a;
+        a = 10;
+
+        cout << "enter your number:" << endl;
+        cout << "enter your number 2:" << endl;
+        int v; //v=number2
+        v = 10;
+        cin >> s;
+        cin >> v;
+        int d_sum;
+        d_sum = v + s * 2;
+
+
+        cout << "the result is:" << d_sum;
+
+        //A+b+a
+        if (v == 10)
+        { //V--1*2*s if v or s > 1-6 units
+            v--;//v-1*2*s or 2*s+v
+
+            d_sum = v-- * 2;//number2-1*2+number1
+
+        }
+        else {
+            false;
+        }
+
+        //std::cout << s;
+
+
+        return 1;
+        using namespace std;
+
+        
+        
+            cout << 'HI' << endl;
+            return 1;
+
+            int m();
+        {
+
+                
+
+                void ok(void); {
+                    cout << "BONJOUR!TU ES BIEN"<< endl;
+            }
+
+        }
+
+    }
+     return 0;
+
+}
+    
+
+
+
+
+
+
+
+
+
+
+
+
+//VECTOR-CHI HUONG  VA GIA TRI CUA NO
